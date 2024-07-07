70ff5c9d707851fafa202ef062a141172238cb70
silvano <snoxdev@github.com>
2024-07-07 14:30:30 -0400

    se agrego funcion multiplicar

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-function suma(a, b){
+function suma(a, b) {
     return a + b;
 }
-
 //multiplicar :: num num -> num
 //Función que multiplica dos número s a y b
 //Ejemplo multiplicar (3, 5) este debe retornar 15
-
-function multiplicar(a:number, b: number): number {
-    let resultado: number = 0;
+function multiplicar(a, b) {
+    var resultado = 0;
     resultado = a * b;
-    return resultado
+    return resultado;
 }
-
 //test
 console.assert(multiplicar(5, 4) == 20);
